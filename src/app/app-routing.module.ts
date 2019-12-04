258bd028409571f46d58d2512f02281465c87a8d
gmujica <gregorymujica1993@gmail.com>
2019-12-04 16:32:00 -0500

    route systen added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultModule } from './layouts/default/default.module';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 
 const routes: Routes = [{
@@ -11,6 +12,10 @@ const routes: Routes = [{
   children: [{
     path: '',
     component: DashboardComponent
+  }, {
+    path: 'posts',
+    component: PostsComponent
+
   }]
 }];
 
