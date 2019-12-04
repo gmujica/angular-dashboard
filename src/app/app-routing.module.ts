ac0dd2d669ebefa7c9307def3a643e0e83f8d470
gmujica <gregorymujica1993@gmail.com>
2019-12-04 14:27:31 -0500

    adding a simple module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultModule } from './layouts/default/default.module';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path: '',
+    component: DashboardComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
