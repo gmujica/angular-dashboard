4f55a15046c307d1488b0ded62fc8212275ff8f6
gmujica <gregorymujica1993@gmail.com>
2019-12-04 19:42:42 -0500

    sidebar slide event added

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  toggleSidebar() {
+    this.toggleSidebarForMe.emit();
   }
 
 }
